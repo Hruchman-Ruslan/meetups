9fd8f7becd9571c3afb9b512e4fab1df708b4b2d
Ruslan <Giovingo92@gmail.com>
2024-08-07 19:53:02 +0300

    feat: adding dynamic pages getStaticProps and getStaticPaths

--- src/pages/[meetupId]/index.tsx
+++ src/pages/[meetupId]/index.tsx
@@ -1,3 +1,5 @@
+import type { GetStaticPropsContext } from "next";
+
 import MeetupDetail from "@/components/meetups/meetup-detail";
 
 export interface MeetupsDetailsPageProps {}
@@ -11,4 +13,43 @@ export default function MeetupsDetailsPage({}: MeetupsDetailsPageProps) {
       description="This is a first meetup!"
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params?.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg ",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some address 5, 12345 Some City",
+        description: "This is a first meetup!",
+      },
+    },
+  };
 }
