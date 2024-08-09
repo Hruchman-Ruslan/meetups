0dc5839041807e2fad9db406519fb99a8898aebd
Ruslan <Giovingo92@gmail.com>
2024-08-09 15:36:57 +0300

    fix: fallback

--- src/pages/[meetupId]/index.tsx
+++ src/pages/[meetupId]/index.tsx
@@ -48,7 +48,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map(({ _id }) => ({
       params: {
         meetupId: _id.toString(),
